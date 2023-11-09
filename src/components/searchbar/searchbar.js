6c3440afb23caf7d72e67d61de6b93804b4f2f7c
soukarja <soukarjadutta@gmail.com>
2023-11-09 21:04:11 +0530

    Added Searchbar Design

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -4,34 +4,36 @@ import "./searchbar.css";
 const SearchBar = () => {
   return (
     <>
-      <form>
+      <form className="searchbar">
         <input type="text" name="name" placeholder="Search Anything..." />
-        <div className="col">
-        <label>Component Type</label>
-        <select>
-          <option selected value="code">
-            Code
-          </option>
-          <option value="design">Design</option>
-        </select>
-        </div>
-        <div className="col">
-        <label>Component Category</label>
-        <select>
-          <option selected hidden value="none">
-            Select a category
-          </option>
-          <option value="views">Views</option>
-          <option value="model">Model</option>
-          <option value="controller">Controllers</option>
-          <option value="class">Class</option>
-          <option value="object">Object</option>
-          <option value="data">Data Access Object</option>
-          <option value="services">Services</option>
-          <option value="plugins">Plugins</option>
-          <option value="api">APIs</option>
-          <option value="others">Others</option>
-        </select>
+        <div className="row">
+          <div className="col">
+            <label>Component Type</label>
+            <select>
+              <option selected value="code">
+                Code
+              </option>
+              <option value="design">Design</option>
+            </select>
+          </div>
+          <div className="col">
+            <label>Component Category</label>
+            <select>
+              <option selected hidden value="none">
+                Select a category
+              </option>
+              <option value="views">Views</option>
+              <option value="model">Model</option>
+              <option value="controller">Controllers</option>
+              <option value="class">Class</option>
+              <option value="object">Object</option>
+              <option value="data">Data Access Object</option>
+              <option value="services">Services</option>
+              <option value="plugins">Plugins</option>
+              <option value="api">APIs</option>
+              <option value="others">Others</option>
+            </select>
+          </div>
         </div>
       </form>
     </>
